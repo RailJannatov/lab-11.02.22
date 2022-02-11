9a0bd1efc197943edf2fb7740ce2355a74a82748
RailJannatov <railcennetov@gmail.com>
2022-02-11 14:15:36 +0400

    lab-tasklar

--- lab 11.02.2022/lab1/lab1/Program.cs
+++ lab 11.02.2022/lab1/lab1/Program.cs
@@ -7,44 +7,61 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            // task 1
+            #region task1
             //int A = Convert.ToInt32(Console.ReadLine());
             //int B = Convert.ToInt32(Console.ReadLine());
             //int Sum = A;
-            //for (int i = 1; i < B; i+=2)
+            //for (int i = 1; i < B; i++)
             //{
+            //    Sum = Sum * A;
 
-            //    Sum *= A;
-            //    //Sum = Sum * A;
+
             //}
             //Console.WriteLine(Sum);
-            //Console.WriteLine(  CheckNumberInDigit(421,9));
 
 
+            #endregion
+
+            #region task2
             //int num1 = 2;
-            //int num2 = 16;
+            //int num2 = 32;
 
             //int sum = 0;
-            //while (num2>=num1)
+            //while (num2 >= num1)
             //{
             //    num2 /= num1;
             //    sum++;
             //}
             //Console.WriteLine(sum);
 
-            //int A = Convert.ToInt32(Console.ReadLine());
-            //int tameded = Convert.ToInt32(Console.ReadLine());
-            //int i;
-            //for (i = 1; tameded > A; i++)
-            //{
-            //    tameded /= A;
-            //}
-            //Console.WriteLine(i);
 
-            Console.WriteLine(CheckFactorialNumber(5));
+            #endregion
+
+            //Console.WriteLine(CheckFactorialNumber(5));
+            //Console.WriteLine(  CheckNumberInDigit(421,9));
+
+            #region task3
+
+
+
+
+
+            //int[] array = { 1, 5, 7, 9, 14,4 };
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum = sum + array[i];
+            //}
+            //Console.WriteLine(sum / array.Length);
+
+
+            #endregion
+
         }
 
-        //static Boolean CheckNumberInDigit(int digit,int number)
+
+        #region task4
+        //static Boolean CheckNumberInDigit(int digit, int number)
         //{
         //    while (digit != 0)
         //    {
@@ -53,19 +70,30 @@ namespace lab1
         //            return true;
         //        }
         //        digit /= 10;
-                
-        //    }
 
+
+
+        //    }
         //    return false;
         //}
-        static int CheckFactorialNumber(int num)
-        {
-            int factorial = 1;
-            for (int i = 1; i <= num; i++)
-            {
-                factorial = factorial*i;
-            }
-            return factorial;
-        }
+
+        #endregion
+
+
+
+
+        #region task5
+        //   
+        //}
+        //static int CheckFactorialNumber(int num)
+        //{
+        //    int factorial = 1;
+        //    for (int i = 1; i <= num; i++)
+        //    {
+        //        factorial = factorial * i;
+        //    }
+        //    return factorial;
+        //}
+        #endregion
     }
 }
